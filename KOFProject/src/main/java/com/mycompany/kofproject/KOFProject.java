0044ef68db8c342f3e37096fe35fa283306632a4
navilex <rd.nightcrawler74@gmail.com>
2025-09-01 23:41:12 -0600

    Mi_primer_respaldo

---
+++ KOFProject/src/main/java/com/mycompany/kofproject/KOFProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kofproject;
+
+import com.mycompany.kofproject.vistas.Login;
+
+/**
+ *
+ * @author Ivan De la Rosa
+ */
+public class KOFProject {
+
+    public static void main(String[] args) 
+    {
+        Login ObjLogin = new Login();
+        ObjLogin.setVisible(true);
+    }
+}
